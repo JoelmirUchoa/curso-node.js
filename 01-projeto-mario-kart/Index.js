4b8d8766f061b72ad9fd5ec5b5d76e1907872a4b
joelmiruchoa <joelmiruchoa@gmail.com>
2026-05-05 21:35:29 -0300

    Adicionando projeto Mario Kart

---
+++ 01-projeto-mario-kart/Index.js
@@ -0,0 +1,15 @@
+const player1 = {
+    nome: "Mario",
+    velocidade: 4,
+    manobrilidade: 3,
+    poder: 3,
+    pontos: 0,
+};
+
+const player2 = {
+    nome: "Luigi",
+    velocidade: 3,
+    manobrilidade: 4,
+    poder: 4,
+    pontos: 0,
+};
