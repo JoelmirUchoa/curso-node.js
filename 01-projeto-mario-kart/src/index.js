1134aa2a6494fafa8d106b6171fa6ca9fd659dbd
trabalho <trabalho@local.com>
2026-05-07 18:12:34 -0300

    Implementar lógica da corrida no jogo Mario Kart

--- 01-projeto-mario-kart/src/index.js
+++ 01-projeto-mario-kart/src/index.js
@@ -18,7 +18,55 @@ async function rollDice() {
     return Math.floor(Math.random() * 6) + 1;
 };
 
-async function playRaceEngine(character1, character2) {}
+async function getRandomBlock() {
+    let random = Math.random();
+    let result;
+
+    switch (true) {
+        case random < 0.33:
+            result = "RETA";  
+            break;
+        case random < 0.66:
+            result = "CURVA";  
+            break;
+        default:
+            result = "CONFRONTO";
+    }
+return result;
+}
+
+async function playRaceEngine(character1, character2) {
+    for (let round = 0; round < 5; round++) {
+        console.log(`🏁 Rodada ${round + 1}`);
+
+        //Sorteio do bloco
+        let block = await getRandomBlock();
+        console.log(`Bloco sorteado: ${block}`);
+    }
+
+    //rolar o dado
+    let diceResult1 = await rollDice();
+    let diceResult2 = await rollDice();
+
+    //teste de habilidade
+    let totalTestSkill1 = 0;
+    let totalTestSkill2 = 0;
+
+    if (block === "RETA") {
+        diceResult1 = diceResult1 + character1.velocidade;
+        diceResult2 = diceResult2 + character2.velocidade;
+    }
+    if (block === "CURVA") {
+        diceResult1 = diceResult1 + character1.manobrilidade;
+        diceResult2 = diceResult2 + character2.manobrilidade;
+    }
+    if (block === "CONFRONTO") {
+        diceResult1 = diceResult1 + character1.poder;
+        diceResult2 = diceResult2 + character2.poder;
+    }
+}
+
+
 
 (async function main() {
     console.log(`🏁🚨Corrida entre ${player1.nome} e ${player2.nome} começando...`
